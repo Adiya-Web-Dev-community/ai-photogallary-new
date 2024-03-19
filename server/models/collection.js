4b7daffd483f68490d68183a675fa4dec6d49684
harshadashirsat6 <harshadaa1997@gmail.com>
2024-03-19 11:48:32 +0530

    event model updated

--- server/models/collection.js
+++ server/models/collection.js
@@ -1,5 +1,7 @@
 const mongoose = requires("mongoose");
 
-const collection_schema = new mongoose.Schema({});
+const collection_schema = new mongoose.Schema({
+    
+});
 
 const collection_model = mongoose.model("collection", collection_schema);
