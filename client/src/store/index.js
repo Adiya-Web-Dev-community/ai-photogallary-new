6b2b3168099f12494662f293b31d1786726c6843
harshadashirsat6 <harshadaa1997@gmail.com>
2024-03-20 15:45:05 +0530

    .

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,12 @@
 import createEventReducer from "./reducer";
 import appReducer from "./app";
+import favouritesReducer from "./favourites";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
     eventPopUP: createEventReducer,
     app: appReducer,
+    favourites: favouritesReducer,
   },
 });
