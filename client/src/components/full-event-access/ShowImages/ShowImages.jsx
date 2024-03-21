2fa62d455e1d2f5ad134cfc7a28ea61f673c5b4a
Ajmalshaikh04 <ajshaikh0404@gmail.com>
2024-03-21 13:12:01 +0530

    ui fix

--- client/src/components/full-event-access/ShowImages/ShowImages.jsx
+++ client/src/components/full-event-access/ShowImages/ShowImages.jsx
@@ -157,7 +157,7 @@ const ShowImages = ({ event }) => {
             <div key={index}>
               <div
                 key={index}
-                className={` pr-16 text-md font-semibold cursor-pointer ${
+                className={` pr-16 text-md font-semibold cursor-pointer select-none ${
                   activeTab === collection.name
                     ? "text-blue-700 font-bold"
                     : "text-gray-500"
