f82890c68255bd0aeb7bbba69fda7d0f562b8bc9
harshadashirsat6 <harshadaa1997@gmail.com>
2024-03-22 11:40:47 +0530

    fetch favourites album section for photographer

--- server/models/favourites.js
+++ server/models/favourites.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const favourites_schema = new mongoose.Schema({
   email: { type: String },
   images: [{ type: String }],
+  eventId: { type: mongoose.Schema.Types.ObjectId, ref: "event" },
 });
 
 favourite_model = mongoose.model("favourite-collection", favourites_schema);
